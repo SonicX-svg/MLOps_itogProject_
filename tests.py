f44bdd0402ec1de46317eeaf079f3abef7f68ac7
SonicX-svg <143742185+SonicX-svg@users.noreply.github.com>
2024-05-18 06:25:55 +0300

    Add files via upload

---
+++ tests.py
@@ -0,0 +1,10 @@
+#________________________________________test____________________________________________
+import pytest
+
+def test_data():
+    assert not df.isnull().sum()
+    
+def test_accuracy():
+    assert >55 
+    
+
