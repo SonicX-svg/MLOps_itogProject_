8b14df4b6d59f814e17f363bfc17bcaa8e5d8385
SonicX-svg <143742185+SonicX-svg@users.noreply.github.com>
2024-05-19 16:58:09 +0300

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -3,11 +3,6 @@ import pytest
 import os
 import sys
 from pathlib import Path
-print(os.path.dirname(__file__))
-parent_dir = Path(os.path.dirname(__file__)).parent
-print(parent_dir)
-sys.path.append(parent_dir)
-print(os.path.abspath(os.curdir))
 os.chdir("..")
 print('os.path.abspath(os.curdir)', os.path.abspath(os.curdir))
 print('sys.path', sys.path)
