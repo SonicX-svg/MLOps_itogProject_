d217ff09f6b62d6876be754e111a9b2b36f223c8
SonicX-svg <143742185+SonicX-svg@users.noreply.github.com>
2024-05-19 17:10:59 +0300

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -3,13 +3,16 @@ import pytest
 import os
 import sys
 from pathlib import Path
-sys.path.append('..')
-print(sys.path)
-print('os.path.abspath(os.curdir)', os.path.abspath(os.curdir))
 
+# Расчёт пути к родительской директории
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-from ..model_preprocessing import df
-from ..test_model import model_accuracy
+# Проверка наличия родительской директории в sys.path
+if parent_dir not in sys.path:
+    sys.path.append(parent_dir)
+
+from model_preprocessing import df
+from test_model import model_accuracy
 
 def test_data():
     assert not df.isnull().sum()
