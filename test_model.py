bcbf60be37b4a4d6da6593a0b44d97fa20f34220
SonicX-svg <143742185+SonicX-svg@users.noreply.github.com>
2024-05-19 11:17:16 +0300

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -10,7 +10,7 @@ from sklearn.metrics import classification_report, confusion_matrix
 # load
 with open('data/model_knn/neigh_model.pkl', 'rb') as f:
     neigh_model = pickle.load(f)
-    
+model_accuracy = accuracy_score(y_true, y_pred)     
 y_pred = neigh_model.predict(X_test)
 print(confusion_matrix(y_test, y_pred)) 
 print(classification_report(y_test, y_pred))
