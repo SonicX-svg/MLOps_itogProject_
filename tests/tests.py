a0b6e1a3f9faeb9339ff177fa0490e6358972cee
SonicX-svg <143742185+SonicX-svg@users.noreply.github.com>
2024-05-19 14:00:07 +0300

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -7,7 +7,11 @@ print(os.path.dirname(__file__))
 parent_dir = Path(os.path.dirname(__file__)).parent
 print(parent_dir)
 sys.path.append(parent_dir)
+print(os.path.abspath(os.curdir))
+os.chdir("..")
+print(os.path.abspath(os.curdir))
 print(sys.path)
+
 from model_preprocessing import df
 from test_model import model_accuracy
 
